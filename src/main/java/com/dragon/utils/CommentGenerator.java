50ea1f2fb05cd59156bb9f0ae005cbf690064752
温文星 <18475536452@163.com>
2019-03-24 23:36:53 +0800

    生成model，字段增加注释

---
+++ src/main/java/com/dragon/utils/CommentGenerator.java
@@ -0,0 +1,28 @@
+package com.dragon.utils;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * 
+ * <p>Title: CommentGenerator</p>  
+ * <p>Description: 生成model，字段增加注释</p>  
+ * @author Dragon.Wen
+ * @date Oct 17, 2018
+ */
+public class CommentGenerator extends DefaultCommentGenerator {
+
+	@Override
+	public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+		super.addFieldComment(field, introspectedTable, introspectedColumn);
+		if (introspectedColumn.getRemarks() != null && !"".equals(introspectedColumn.getRemarks())) {
+			field.addJavaDocLine("/**");
+			field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+			addJavadocTag(field, false);
+			field.addJavaDocLine(" */");
+		}
+	}
+
+}
