6a35009ee246a2cfeb33f18b390b39b3b2a03b0c
温文星 <18475536452@163.com>
2019-03-25 15:21:20 +0800

    自动生成代码例子

---
+++ src/main/java/com/dragon/blog/rpc/api/BlogMenuService.java
@@ -0,0 +1,13 @@
+package com.dragon.blog.rpc.api;
+
+import com.dragon.blog.base.BaseService;
+import com.dragon.blog.dao.model.BlogMenu;
+import com.dragon.blog.dao.model.BlogMenuExample;
+
+/**
+* BlogMenuService接口
+* Created by Dragon.Wen on 2019/3/25.
+*/
+public interface BlogMenuService extends BaseService<BlogMenu, BlogMenuExample> {
+
+}
